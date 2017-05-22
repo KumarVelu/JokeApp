20bfa7c94bfcd6a4ceeb43139a5b3c1fc79a2d3b
KumarVelu <kumar_velu@thbs.com>
2017-05-22 18:09:54 +0530

    Create Android library that displays joke

---
+++ displaylib/src/main/java/com/velu/displaylib/DisplayActivity.java
@@ -0,0 +1,22 @@
+package com.velu.displaylib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    TextView mTvJoke;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+
+        mTvJoke = (TextView) findViewById(R.id.joke);
+
+        String joke = getIntent().getStringExtra("joke");
+
+        mTvJoke.setText(joke);
+    }
+}
